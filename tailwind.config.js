878fee1f275c42e8a51d7e18f68de24cd383f264
blazte <blazte.aster@gmail.com>
2023-07-13 11:47:41 -0500

    add font size, weights and family into tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        extend: {
+            spacing: {
+                '8p': '8px',
+                '12p': '12px',
+                '16p': '16px',
+                '22p': '22px',
+            },
+            fontSize: (theme) => ({
+                xs: '0.75rem',
+                sm: '0.875rem',
+                base: '1rem',
+                md: '1.125rem',
+                lg: '1.5rem',
+                xl: '1.625rem',
+                '2xl': '2rem',
+                ...theme('spacing'),
+            }),
+        },
+        fontFamily: {
+            serif: ['Quicksand'],
+        },
+        fontWeight: {
+            meidum: '500',
+            bold: '700',
+        },
         colors: {
             transparent: 'transparent',
             black: '#000',
