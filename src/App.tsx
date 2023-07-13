b473bb7c600e09a06edb26f64b7c1265af4a55cf
blazte <blazte.aster@gmail.com>
2023-07-12 19:43:41 -0500

    add colors configuration to tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Prueba</h1>
+            <h1 className="text-3xl font-bold underline text-yellow-700 mb-3">
+                Prueba
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+                veritatis, rerum laboriosam maiores odit cupiditate
+                exercitationem non tempora error debitis.
+            </p>
         </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        colors: {
+            transparent: 'transparent',
+            black: '#000',
+            white: '#fff',
+            yellow: {
+                100: '#fff0de',
+                200: '#fee3bb',
+                300: '#fdd697',
+                400: '#fcc974',
+                600: '#fbbb50',
+                500: '#faae2d',
+                700: '#f9a109',
+            },
+            purple: {
+                100: '##FAFAFE',
+            },
+            pink: {
+                700: '#80485B',
+            },
+            blue: {
+                400: '#56CCF2',
+            },
+            red: {
+                500: '#EB5757',
+            },
+            gray: {
+                200: '#C1C1C4',
+                300: '#BDBDBD',
+                400: '#828282',
+                700: '#454545',
+                800: '#34333A',
+            },
+        },
     },
     plugins: [],
 };
