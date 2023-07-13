b02e4bf0b8164468c626b562230650820bb66772
blazte <blazte.aster@gmail.com>
2023-07-13 12:47:50 -0500

    install quicksand font faimily

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline text-yellow-700 mb-3">
+            <h1 className="text-3xl font-bold underline text-yellow-700  font-serif">
                 Prueba
             </h1>
-            <p>
+            <h1 className="text-3xl font-bold undeline text-yellow-700 font">
+                Prueba
+            </h1>
+            <p className="text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
                 veritatis, rerum laboriosam maiores odit cupiditate
                 exercitationem non tempora error debitis.
